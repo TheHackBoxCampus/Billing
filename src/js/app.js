1da7676f07a5ae549db11fc4ab1f3e906f99a09e
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-06-02 09:24:05 -0500

    feat: :sparkles: Avance de plantilla de formulario

---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+import "./components/billing_header.js";
---
+++ src/js/components/billing_header.js
@@ -0,0 +1,17 @@
+export default class BILLING_HEADER extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static async get_component() {
+        return await (await fetch('src/js/components/templates/billing.html')).text(); 
+    }
+
+    connectedCallback() {
+        Promise.resolve(BILLING_HEADER.get_component()).then(html => 
+            this.innerHTML = html
+        ); 
+    }
+}
+
+customElements.define('billing-header', BILLING_HEADER); 
